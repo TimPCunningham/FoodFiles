23cc9f01651b6de5f1bcf84bc864ffb2811498bb
Tim Cunningham <timothypaulcunningham@gmail.com>
2023-03-04 20:50:05 -0500

    Update webpack configs

---
+++ webpack.production.config.cjs
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  watch: false,
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource"
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  }
+};
